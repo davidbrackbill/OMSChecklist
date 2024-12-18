63eeb374751b2049eb98b45cd364271f17cc8423
David Brackbill <dmbrackbill@gmail.com>
2024-12-18 11:05:46 -0800

    pull table out to component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,10 @@
 <script>
     import { specs, course_codes, sorted_courses } from "../lib/data.js";
-    import Semesters from "$lib/semesters.svelte";
     import Buckets from "$lib/buckets.svelte";
-
-    let sort_criteria = ["Reviews", "desc"];
-    function sort(column) {
-        let [c, s] = sort_criteria;
-        if (c == column) {
-            sort_criteria[1] = s === "desc" ? "asc" : "desc";
-        } else {
-            sort_criteria = [column, "desc"];
-        }
-    }
+    import Semesters from "$lib/semesters.svelte";
+    import Table from "$lib/table.svelte";
 
     let active_courses = new Set();
-    const clear_courses = () => {
-        active_courses.clear();
-        active_courses = active_courses;
-    };
-    function toggle_courses(item) {
-        active_courses.has(item)
-            ? active_courses.delete(item)
-            : active_courses.add(item);
-        active_courses = active_courses;
-    }
 
     let active_specs = new Set(["Computing Systems"]);
     function toggle_specs(item) {
@@ -46,10 +27,6 @@
         }
     }
 
-    function row_class(code, active_courses) {
-        if (active_courses.has(code)) return "b-highlight";
-        return "";
-    }
 </script>
 
 <div class="flexw mb-20">
@@ -76,49 +53,4 @@
 
 <Semesters {active_courses} />
 
-<div class="flexcenter mt--10">
-    <b class="m-5">Courses:</b>
-    <p>{active_courses.size}</p>
-    <button class="thickbutton" on:click={clear_courses}>Clear</button>
-    {#each active_courses as Code}
-        <button on:click={() => toggle_courses(Code)}>{Code}</button>
-    {/each}
-</div>
-
-<table cellspacing="0" cellpadding="0">
-    <tr class="bold">
-        <td on:click={() => sort("Code")}>Code</td>
-        <td on:click={() => sort("Course")}>Course</td>
-        <td on:click={() => sort("Rating")}>Rating</td>
-        <td on:click={() => sort("Difficulty")}>Difficulty</td>
-        <td on:click={() => sort("Workload")}>Workload</td>
-        <td on:click={() => sort("Reviews")}>Reviews</td>
-    </tr>
-    {#each sorted_courses[sort_criteria.join("_")] as { Course, Code, Rating, Difficulty, Workload, Reviews }}
-        {#if active_table_rows.has(Code)}
-            <tr
-                on:click={() => toggle_courses(Code)}
-                class={row_class(Code, active_courses)}
-            >
-                <td><button>{Code}</button></td>
-                <td>{Course}</td>
-                <td>{Rating}</td>
-                <td>{Difficulty}</td>
-                <td>{Workload}</td>
-                <td>{Reviews}</td>
-            </tr>
-        {/if}
-    {/each}
-</table>
-
-<style>
-    .thickbutton {
-        border-width: 2px;
-        border-radius: 5px;
-    }
-
-    td {
-        padding: 2px;
-        min-width: 100px;
-    }
-</style>
+<Table bind:active_courses {active_table_rows} />
