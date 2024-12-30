3927bf5b2b68d68c57341eee21c6bedc5cb41576
David Brackbill <dmbrackbill@gmail.com>
2024-12-30 14:48:58 -0800

    fix table column sizes and alignment

--- src/lib/table.svelte
+++ src/lib/table.svelte
@@ -36,14 +36,17 @@
 </script>
 
 <table
-    class="basis-1/2 shrink self-baseline justify-self-end max-h-screen overflow-y-auto rounded-lg border-2 border-gray-300"
+    class="basis-1/2 shrink grow self-baseline justify-self-end max-h-screen overflow-y-scroll no-sb rounded-lg shadow"
 >
+    {#each ["15%", "55%", "10%", "10%", "10%"] as width}
+	<col style={`width:${width}`}>
+    {/each}
     <thead class="bg-gray-100">
         <tr>
-            {#each columns as column}
+            {#each columns as column, i}
                 <th
-                    align="left"
-                    class="text-gray-600 font-normal"
+                    align={i < 2 ? "left" : "right"}
+                    class="text-gray-600 font-normal pr-2"
                     on:click={() => toggle(column)}>{column}</th
                 >
             {/each}
@@ -72,12 +75,19 @@
 </table>
 
 <style>
-    td {
-        vertical-align: top;
-    }
     table {
         border-collapse: separate;
         border-spacing: 0;
+    }
+
+    td,
+    th {
+        padding: 0 0.5rem;
+        max-width: 20rem;
+        vertical-align: top;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
     }
 
     /* Right-align the numbers */
